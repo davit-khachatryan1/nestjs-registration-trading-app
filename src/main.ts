612b52d8f873bbdb19e52ea65e2956c1582cba85
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-10-02 22:38:09 +0400

    push

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // Allow only this origin
+    origin: 'https://web3-calculator.vercel.app', // Allow only this origin
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these HTTP methods
     credentials: true, // Allow cookies to be sent with requests
   });
